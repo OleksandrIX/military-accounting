98b4772bb292b84c91fc232f7b84c762e7b41bef
OleksandrIX <zagorenkosasha@gamil.com>
2023-06-24 11:48:06 +0300

    Add modal equipment

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -1,7 +1,11 @@
 import React, {useEffect, useState} from "react";
-import {DataGrid} from '@mui/x-data-grid';
-import axios from "axios";
 import serverAPI from "./util/server.api";
+import axios from "axios";
+import {DataGrid} from '@mui/x-data-grid';
+
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
 
 const columns = [
     {field: 'name', headerName: 'Name', width: 150},
@@ -34,6 +38,9 @@ function Main() {
         setAddModal(true);
     }
 
+    const closeAddModal = () => {
+        setAddModal(false);
+    }
 
     return (
         <div className="App">
@@ -41,34 +48,60 @@ function Main() {
             <div className="Table" style={{height: 300, width: "fit-content"}}>
                 <DataGrid rows={equipments} columns={columns}/>
             </div>
-            <div className="button add" onClick={openAddModal}>Add</div>
-
-            <div className="modal">
-                {
-                    addModal ?
-                        <AddModal id="add-modal"></AddModal>
-                        :
-                        ""
-                }
-            </div>
+            <Button onClick={openAddModal}>Open Add Modal</Button>
+            <Modal open={addModal}
+                   onClose={closeAddModal}
+            >
+                <Box sx={{
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    width: 400,
+                    background: 'white',
+                    border: '2px solid #000',
+                    boxShadow: 24,
+                    p: 2
+                }}>
+                    <AddModal></AddModal>
+                </Box>
+            </Modal>
+            <a href="/statistic">Statistic</a>
         </div>
     );
 }
 
 function AddModal({}) {
-    const [addModal, setAddModal] = useState(true);
-
-    if (addModal) {
-        document.querySelector("#add-modal").style.display = "flex"
-    }
-
-    const closeAddModal = () => {
-        setAddModal(false);
-    }
     return (
         <div className="modalContent">
-            <span className="close" onClick={closeAddModal}>&times;</span>
             <p className="nameModal">Add equipment</p>
+            <form className="form">
+                <label htmlFor="name">
+                    Name:
+                    <input id="name" name="name" type="text" required autoFocus={true}/>
+                </label>
+                <label htmlFor="type">
+                    Type:
+                    <input id="type" name="type" type="text" required/>
+                </label>
+                <label htmlFor="description">
+                    Description:
+                    <input id="description" name="description" type="text" required/>
+                </label>
+                <label htmlFor="country">
+                    Country:
+                    <input id="country" name="country" type="text" required/>
+                </label>
+                <label htmlFor="countOfEquipment">
+                    Count Of Equipment:
+                    <input id="countOfEquipment" name="countOfEquipment" type="text" required/>
+                </label>
+                <label htmlFor="date">
+                    Date:
+                    <input id="date" name="date" type="date" required/>
+                </label>
+                <input className="button add" type="submit" value="Add"/>
+            </form>
         </div>
     )
 }
