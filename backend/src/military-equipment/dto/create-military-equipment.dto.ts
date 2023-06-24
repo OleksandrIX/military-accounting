415d13da8ff8613489f688c1963c079155b034db
Denis <77487610+Denis11333@users.noreply.github.com>
2023-06-24 10:47:07 +0300

    init

--- backend/src/military-equipment/dto/create-military-equipment.dto.ts
+++ backend/src/military-equipment/dto/create-military-equipment.dto.ts
@@ -3,6 +3,6 @@ export class CreateMilitaryEquipmentDto{
   readonly name: string;
   readonly type: string;
   readonly description: string;
-  readonly countOfEquipment: string;
+  readonly countOfEquipment: number;
   readonly date: string
 }
