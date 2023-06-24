1053cc628676f6008b2385640f57ddaac393848e
OleksandrIX <zagorenkosasha@gamil.com>
2023-06-24 11:19:27 +0300

    Add modal

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -18,6 +18,7 @@ const getEquipments = async (url) => {
 
 function Main() {
     const [equipments, setEquipments] = useState([]);
+    const [addModal, setAddModal] = useState(false);
 
     useEffect(() => {
         getEquipments(serverAPI.equipments)
@@ -29,14 +30,47 @@ function Main() {
             .catch(e => console.log(e));
     }, []);
 
+    const openAddModal = () => {
+        setAddModal(true);
+    }
+
+
     return (
         <div className="App">
             <h2>Military Equipment</h2>
             <div className="Table" style={{height: 300, width: "fit-content"}}>
                 <DataGrid rows={equipments} columns={columns}/>
             </div>
+            <div className="button add" onClick={openAddModal}>Add</div>
+
+            <div className="modal">
+                {
+                    addModal ?
+                        <AddModal id="add-modal"></AddModal>
+                        :
+                        ""
+                }
+            </div>
         </div>
     );
+}
+
+function AddModal({}) {
+    const [addModal, setAddModal] = useState(true);
+
+    if (addModal) {
+        document.querySelector("#add-modal").style.display = "flex"
+    }
+
+    const closeAddModal = () => {
+        setAddModal(false);
+    }
+    return (
+        <div className="modalContent">
+            <span className="close" onClick={closeAddModal}>&times;</span>
+            <p className="nameModal">Add equipment</p>
+        </div>
+    )
 }
 
 export default Main
